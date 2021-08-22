472dc41982104ff2bb75fe3b8ff555c80520cb4f
nousher <nousheramir99@gmail.com>
2021-08-22 14:46:32 -0400

    Added new form API description

--- inputform/generate-api.sh
+++ inputform/generate-api.sh
@@ -1,4 +1,4 @@
-oapi-codegen -generate types -package payment input-form.yaml > openapi_types.gen.go
-oapi-codegen -generate server -package payment input-form.yaml > openapi_server.gen.go
-oapi-codegen -generate spec -package payment input-form.yaml > openapi_spec.gen.go
-oapi-codegen -generate client -package payment input-form.yaml > openapi_client.gen.go
+oapi-codegen -generate types -package newForm input-form.yaml > openapi_types.gen.go
+oapi-codegen -generate server -package newForm input-form.yaml > openapi_server.gen.go
+oapi-codegen -generate spec -package newForm input-form.yaml > openapi_spec.gen.go
+oapi-codegen -generate client -package newForm input-form.yaml > openapi_client.gen.go
